4c420dadcaf5e5aded7e1cd58033105d0936033f
Fhmi00 <anisfahmi045@gmail.com>
2022-08-23 14:07:41 +0700

    fix validation task 2

--- task.js
+++ task.js
@@ -52,12 +52,19 @@ const biodata = {
 
 // ---TASK 2---
 
-const mtk = 82;
+const mtk = "85";
 const bIndo = 75;
 const bIng = 75;
 const ipa = 80;
 
-const rataRata = ((mtk+bIndo+bIng+ipa)/4);
+let rataRata;
+
+if(typeof mtk === "number" && typeof bIndo === "number" && typeof bIng === "number" && typeof ipa === "number") {
+    rataRata = (mtk + bIndo + bIng + ipa)/4
+    console.log('Rata-rata = ' + rataRata);
+}else{
+    console.log('invalid value!!');
+}
 
 let grade = rataRata;
 if(grade>=90 && grade<=100) {
@@ -73,8 +80,7 @@ if(grade>=90 && grade<=100) {
 }else{
     grade = "nilai yang anda masukkan salah"
 };
-// console.log('Rata-rata = ' + rataRata);
-// console.log('Grade = ' + grade);
+console.log('Grade = ' + grade);
 
 
 // ---TASK 3---
@@ -85,12 +91,12 @@ let s = '';
 for(i = 1; i <= printSquare; i++) {
     for(b = 1; b <= printSquare; b++) {
         if(i == 1 || i == printSquare) {
-            s += '*'
+            s += '* '
         }else {
             if(b == 1 || b == printSquare) {
-                s += '*'
+                s += '* '
             }else{
-                s += ' '
+                s += '  '
             }
         }
     } 
@@ -122,6 +128,6 @@ let newData = {...data, name: "fahmi", email: "ifahmi5555@gmail.com", hobby: ["M
 // data.email = "ifahmi5555@gmail.com"
 // let newData = {...data, hobby: ["Main Game", "Musik", "Film"]}
 
-console.log(newData);
+// console.log(newData);
 
 let {street, city} = data.address
